ee728b01c115198b6cedbf9eb52b9d2d918e6377
fdiaz <luisfernandodiaz.c2001@gmail.com>
2024-09-08 20:29:15 -0500

    skills

--- js/proyectos.js
+++ js/proyectos.js
@@ -3,7 +3,7 @@ function cambio() {
     c++;
     if (c > 5) c = 1;
     document.getElementById("carrusel").setAttribute("src", "img/" + c + ".jpg");
-    setTimeout("cambio()", 1000);
+    setTimeout("cambio()", 2000);
 }
 document.body.setAttribute("onload", "cambio()");
 
