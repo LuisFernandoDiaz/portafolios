e74f273061eef3f558a4fa878424a36f32e34d13
fdiaz <luisfernandodiaz.c2001@gmail.com>
2024-10-24 22:04:14 -0500

    agregar ads

--- js/proyectos.js
+++ js/proyectos.js
@@ -1,9 +1,3 @@
-var c = 0;
-function cambio() {
-    c++;
-    if (c > 5) c = 1;
-    document.getElementById("carrusel").setAttribute("src", "img/" + c + ".jpg");
-    setTimeout("cambio()", 2000);
-}
-document.body.setAttribute("onload", "cambio()");
+
+
 
